e8f6040c280d36bacca1710a3f780d88fc64ec8b
bagasarjun12 <bagasdivan123@gmail.com>
2023-02-09 14:24:05 +0700

    first commit

---
+++ tes.php
@@ -0,0 +1,88 @@
+<?php
+if($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+    echo "<H1>Tabung saja</H1>";
+    echo "<br>";
+    echo '<img src="'.$nabung.'">';
+// //SAMPAI SINI COPASNYA
+// } else if ($jawaban1=="a1" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+//                 echo "Reksa dana atau Investasi Emas";
+            // } else if ($jawaban1=="a1" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+            //     echo "Investasi Emas (Prioritas) atau menabung";
+            // } else if ($jawaban1=="a1" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+            //     echo "Investasi Emas / Menabung / Reksa Dana";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                echo "Menabung / Reksa Dana";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                echo "Menabung / Reksa Dana";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                echo "Menabung / Reksa Dana / Investasi Emas";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                echo "Menabung / Reksa Dana / Investasi Emas";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                echo "Menabung";
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                echo "Menabung";    
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                echo "Menabung (Prioritas) atau Investasi Emas";   
+            } else if ($jawaban1=="a1" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                echo "Menabung (Prioritas) atau Investasi Emas";   
+            }else if($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Tabung saja";
+                } else if ($jawaban1=="a2" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Reksa dana (Prioritas) atau Investasi Emas (alternatif)";
+                } else if ($jawaban1=="a2" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Reksa dana atau saham";
+                } else if ($jawaban1=="a2" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Reksa dana (Prioritas), emas (Prioritas), Saham";
+                } else if ($jawaban1=="a2" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Reksa dana, emas, Saham";
+                } else if ($jawaban1=="a2" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Emas (Prioritas) atau Reksa dana";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Emas (Prioritas), reksa dana, saham, properti";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Reksa Dana";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Reksa Dana";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Reksa dana atau emas";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Reksa dana, emas, saham";
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Menabung";    
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Menabung";   
+                } else if ($jawaban1=="a2" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Menabung (Prioritas) atau Investasi Emas";  
+                } else if($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Tabung saja";
+                } else if ($jawaban1=="a3" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Reksa dana (Prioritas) atau emas";
+                } else if ($jawaban1=="a3" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Reksa dana, saham, properti";
+                } else if ($jawaban1=="a3" && $jawaban2=="b1" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Reksa dana atau Emas";
+                } else if ($jawaban1=="a3" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Reksa dana, emas, saham, properti";
+                } else if ($jawaban1=="a3" && $jawaban2=="b1" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Emas (Prioritas), reksa dana, properti";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Emas (Prioritas), Properti (alternatif), reksa dana, saham";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Emas (Prioritas), reksa dana, properti";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Emas (Prioritas), Properti (alternatif), reksa dana, saham";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c1" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Reksa dana";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e1"){
+                    echo "Reksa dana";
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d1"  && $jawaban5=="e2"){
+                    echo "Reksa dana atau emas";    
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e1"){
+                    echo "Reksa dana, emas, saham, properti";   
+                } else if ($jawaban1=="a3" && $jawaban2=="b2" && $jawaban3=="c2" && $jawaban4=="d2"  && $jawaban5=="e2"){
+                    echo "Emas, properti, tabung";   
+            } else{
+                echo "Menabung Saja";
+            }
+            ?>
